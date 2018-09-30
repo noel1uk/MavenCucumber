c37c4ddecbee4821542c827f3606fd994cf63d65
Noel Vock <noel1uk@gmail.com>
2018-09-30 22:45:30 +0100

    add print line

--- src/test/java/Tests/LoginStepDefinitions.java
+++ src/test/java/Tests/LoginStepDefinitions.java
@@ -6,33 +6,35 @@ import cucumber.api.java.en.When;
 
 public class LoginStepDefinitions {
 
+    // here we create a map between a java method and a test
     @Given("^A user is on Demoqa\\.com$")
     public void a_user_is_on_Demoqa_com() throws Throwable {
-
+    System.out.println("User is on Demoq.com");
     }
 
     @When("^user clicks on MyAccount link$")
     public void user_clicks_on_MyAccount_link() throws Throwable {
+        System.out.println("User clicked on MyAccountLink");
     }
 
     @Then("^user is taken to login page$")
     public void user_is_taken_to_login_page() throws Throwable {
-
+        System.out.println("User is taken to the login page");
     }
 
     @When("^user enters a valid username and password$")
     public void user_enters_a_valid_username_and_password() throws Throwable {
-
+        System.out.println("User enters valid username and password");
     }
 
     @Then("^user is able to login successfully$")
     public void user_is_able_to_login_successfully() throws Throwable {
-
+        System.out.println("User is able to login successfully");
     }
 
     @Given("^A user is on ToolsQA login page$")
     public void a_user_is_on_ToolsQA_login_page() throws Throwable {
-
+        System.out.println("User is on Demoq.com");
     }
 
 }
