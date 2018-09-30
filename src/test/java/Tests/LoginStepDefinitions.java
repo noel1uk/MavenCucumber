7fb5b2a2d5655724401692d507ec21c9b1066996
Noel Vock <noel1uk@gmail.com>
2018-09-30 23:31:07 +0100

    enter search text and click enter on google

--- src/test/java/Tests/LoginStepDefinitions.java
+++ src/test/java/Tests/LoginStepDefinitions.java
@@ -3,21 +3,29 @@ package Tests;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginStepDefinitions {
+    WebDriver driver = new ChromeDriver();
 
     // here we create a map between a java method and a test
     @Given("^A user is on Demoqa\\.com$")
     public void a_user_is_on_Demoqa_com() throws Throwable {
-        WebDriver driver = new ChromeDriver();
+//        WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.co.uk/");
     System.out.println("User is on Demoq.com");
     }
 
     @When("^user clicks on MyAccount link$")
     public void user_clicks_on_MyAccount_link() throws Throwable {
+        driver.findElement(By.id("lst-ib")).sendKeys("Googling");
+        driver.findElement(By.xpath("//body")).sendKeys(Keys.RETURN);
+        driver.findElement(By.name("btnK")).click();
+
+
         System.out.println("User clicked on MyAccountLink");
     }
 
